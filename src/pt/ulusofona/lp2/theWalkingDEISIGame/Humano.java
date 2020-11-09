55c0abdb78ba63c50599c5107b50031f57c46253
Filipe <snoopy84x@protonmail.com>
2020-11-09 13:29:48 +0000

    Added basic classes: -Humano.java -Zombie.java -TWDGameManager.java

---
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Humano.java
@@ -0,0 +1,9 @@
+package pt.ulusofona.lp2.theWalkingDEISIGame;
+
+public class Humano {
+    int id;
+    int idCriatura = 1;
+    String nome;
+
+
+}
---
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
@@ -0,0 +1,7 @@
+package pt.ulusofona.lp2.theWalkingDEISIGame;
+
+public class TWDGameManager {
+    public TWDGameManager() {
+    }
+
+}
