e7a1c508c257981a81b240426bca7cbde3cfc44e
Filipe <filipezarcocoutinho@gmail.com>
2020-12-28 03:03:58 +0000

    Remove unkwon package

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/Main.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Main.java
@@ -1,7 +1,5 @@
 package pt.ulusofona.lp2.theWalkingDEISIGame;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
 public class Main {
     public static void main(String[] args) {
     }
