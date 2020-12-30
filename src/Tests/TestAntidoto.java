70c0bcf3cf34aa69b5853259fad92d1106dacef8
Tomás <tomasneto2000@gmail.com>
2020-12-30 23:01:57 +0000

    testes venenos. (erro?)

---
+++ src/Tests/TestAntidoto.java
@@ -0,0 +1,16 @@
+package Tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import pt.ulusofona.lp2.theWalkingDEISIGame.TWDGameManager;
+
+import java.io.File;
+
+import java.io.File;
+
+public class TestAntidoto {
+    TWDGameManager gameManager = new TWDGameManager();
+    File fich = new File("test-files/TestAntidoto");
+
+
+}
---
+++ src/Tests/TestVeneno.java
@@ -0,0 +1,37 @@
+package Tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import pt.ulusofona.lp2.theWalkingDEISIGame.TWDGameManager;
+
+import java.io.File;
+
+public class TestVeneno {
+    TWDGameManager gameManager = new TWDGameManager();
+    File fich = new File("test-files/TestVeneno");
+
+    @Test
+    public void ApanhaVenenoMorre(){
+        gameManager.startGame(fich);
+        Assert.assertFalse(gameManager.move(3, 3, 2, 2));
+        Assert.assertFalse(gameManager.move(2, 1, 3, 1));
+        Assert.assertFalse(gameManager.move(6, 6, 6, 5));
+        Assert.assertFalse(gameManager.move(3, 1, 3, 0));
+        Assert.assertFalse(gameManager.move(6, 5, 6, 6));
+        Assert.assertFalse(gameManager.move(3, 0, 3, 1));
+        Assert.assertFalse(gameManager.move(6, 6, 6, 5));
+
+        int id = gameManager.getElementId(2,2);
+        Assert.assertEquals("Id expected to be 1 but was: " + id, 0, id);
+    }
+
+    @Test
+    public void ApanhaVenenoSobrevive(){
+        gameManager.startGame(fich);
+        Assert.assertFalse(gameManager.move(3, 3, 2, 2));
+        Assert.assertFalse(gameManager.move(2, 1, 3, 1));
+
+        int id = gameManager.getElementId(2,2);
+        Assert.assertEquals("Id expected to be 1 but was: " + id, 1, id);
+    }
+}
