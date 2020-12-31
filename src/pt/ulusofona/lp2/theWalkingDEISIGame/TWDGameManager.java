d8f61e089cde5a0ee43fc88502961305d888b20e
Filipe <filipezarcocoutinho@gmail.com>
2020-12-31 03:35:54 +0000

    last commit is wrong

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
@@ -124,7 +124,7 @@ public class TWDGameManager {
                 move(xO,yO,xD,yD);
             }
             scanner.close();
-        } catch (FileNotFoundException | IndexOutOfBoundsException e) {
+        } catch (FileNotFoundException e) {
             return false;
         }
         return true;
@@ -240,7 +240,7 @@ public class TWDGameManager {
                 return false;
             }
 
-        } catch (FileNotFoundException | IndexOutOfBoundsException e) {
+        } catch (FileNotFoundException e) {
             return false;
         }
         return true;
@@ -423,7 +423,7 @@ public class TWDGameManager {
         resultado[0] = "Resident Evil";
         resultado[1] = "Evil Dead";
         resultado[2] = "I Am Legend";
-        resultado[3] = "Dawn of the Dead";
+        resultado[3] = "Blade";
         resultado[4] = "Freaky Stories";
         resultado[5] = "The Return of the Living Dead";
         resultado[6] = "The Mandalorian";
