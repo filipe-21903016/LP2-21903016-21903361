b09bcacaad5a4c24fbb7ad07e9c0a301bdbfe9f6
Tomás <tomasneto2000@gmail.com>
2021-01-02 18:50:39 +0000

    pop culture militar another attempt x2

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
@@ -445,7 +445,7 @@ public class TWDGameManager {
         resultado[1] = "Evil Dead";
         resultado[2] = "I Am Legend";
         resultado[3] = "I Am Legend";
-        resultado[4] = "Castlevania";
+        resultado[4] = "Popeye the Sailor";
         resultado[5] = "The Return of the Living Dead";
         resultado[6] = "The Mandalorian";
         resultado[7] = "1996";
